c6c7ce94ed11710198679579b59fea93ff0eabb0
josuafalguera <21-1-00563@vsu.edu.ph>
2024-03-15 13:51:24 +0800

    Added a line on top of the navigation bar for separation with the screen.

--- bayad_system/lib/navigation_menu.dart
+++ bayad_system/lib/navigation_menu.dart
@@ -1,3 +1,4 @@
+import 'package:bayad_system/features/inventory/screens/product.dart';
 import 'package:bayad_system/utils/helpers/helper_functions.dart';
 import 'package:bayad_system/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
@@ -15,24 +16,29 @@ class NavigationMenu extends StatelessWidget {
 
     return Scaffold(
       bottomNavigationBar: Obx(
-        () => NavigationBar(
-          height: 80,
-          elevation: 0,
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: (index) => controller.selectedIndex.value = index,
-          backgroundColor: darkMode ? CustomColors.black : CustomColors.white,
-          indicatorColor: darkMode ? CustomColors.white.withOpacity(0.2) : CustomColors.black.withOpacity(0.2),
-        
-          destinations: const [
-            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Iconsax.box), label: 'Inventory'),
-            NavigationDestination(icon: Icon(Iconsax.menu_board), label: 'Products'),
-            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-          ],
+        () => Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: darkMode ? Colors.white.withOpacity(0.2) : Colors.black.withOpacity(0.2), width: 0.5),
+            ),
+          ),
+          child: NavigationBar(
+            height: 80,
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) => controller.selectedIndex.value = index,
+            backgroundColor: darkMode ? CustomColors.black : CustomColors.white,
+            indicatorColor: darkMode ? CustomColors.white.withOpacity(0.2) : CustomColors.black.withOpacity(0.2),
+            destinations: const [
+              NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Iconsax.box), label: 'Inventory'),
+              NavigationDestination(icon: Icon(Iconsax.menu_board), label: 'Products'),
+              NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            ],
+          ),
         ),
       ),
-      
-      body:Obx(() => controller.screens[controller.selectedIndex.value]),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
 }
@@ -43,9 +49,7 @@ class NavigationController extends GetxController{
   final screens = [
     Container(color: Colors.red),
     Container(color: Colors.green),
-    Container(color: Colors.blue),
+    const ProductScreen(),
     Container(color: Colors.yellow),
   ];
-
-
 }
